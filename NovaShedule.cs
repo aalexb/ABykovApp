0993bb6a34cba39cead6264356cab40cf007c931
Алексей Быков <alexeibykov72@gmail.com>
2022-10-03 17:43:14 +0500

    2022-10-03w

---
+++ NovaShedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace WorkApp.Addons
+{
+    internal class NovaShedule
+    {
+        int Col;
+        int Row;
+        ViewSchedule shed;
+
+        public NovaShedule(int col, int row)
+        {
+            Col = col;
+            Row = row;
+        }
+        public void DrawToRevit()
+        {
+            shed.GetTableData().GetSectionData();
+        }
+
+}
